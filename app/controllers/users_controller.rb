717ca7c115b5ddf0dee2c08c9d836d446e3cf5d0
baqar-abbas <baqar007@hotmail.com>
2023-09-05 23:25:24 +0500

    Created a partial for header. Created an action for logout. Signup, login and logout all functionalities are working fine.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
     def index
     end
+
+    def show
+    end
+
+    def logout
+        sign_out current_user
+        redirect_to root_path
+      end
 end
