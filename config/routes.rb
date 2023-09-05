dab6ab66ac3702d124484ab49e814381e494257c
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-09-05 18:23:32 +0330

    Modify the routes to set the root_path.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "main#index"
+  # devise_scope :users do 
+  # end
+
+  root "users#index"
+  
+  resources :users do
+  end
 end
