0979a9f5ee1e958284553e9d2e58f4853b2bb1f1
baqar-abbas <baqar007@hotmail.com>
2023-09-05 23:37:05 +0500

    Linters errors removed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,10 @@
 class UsersController < ApplicationController
-    def index
-    end
+  def index; end
 
-    def show
-    end
+  def show; end
 
-    def logout
-        sign_out current_user
-        redirect_to root_path
-      end
+  def logout
+    sign_out current_user
+    redirect_to root_path
+  end
 end
