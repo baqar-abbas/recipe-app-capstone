16790a058c907df6268ff99387faee5a1712ba13
baqar-abbas <baqar007@hotmail.com>
2023-09-07 07:23:25 +0500

    updated routes for food list

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-
-  # devise_scope :users do 
-  # end
-
+ 
   root "users#index"
 
   resources :users do
     resources :recipes, except: [:update] do
+    end
+    resources :foods, except: [:update] do
     end
   end
 end
