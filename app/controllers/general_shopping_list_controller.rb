e23b7dbb967915d521477abc552a83a65bc3cc9f
baqar-abbas <baqar007@hotmail.com>
2023-09-08 22:58:03 +0500

    Linters errros fixed

--- app/controllers/general_shopping_list_controller.rb
+++ app/controllers/general_shopping_list_controller.rb
@@ -1,10 +1,10 @@
 class GeneralShoppingListController < ApplicationController
-    before_action :authenticate_user!
-    def index
-      @foods = Food.all
-      @total = 0
-      current_user.foods.each do |food|
-        @total += food.price * food.quantity
-      end
+  before_action :authenticate_user!
+  def index
+    @foods = Food.all
+    @total = 0
+    current_user.foods.each do |food|
+      @total += food.price * food.quantity
     end
+  end
 end
